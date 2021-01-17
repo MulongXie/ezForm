6126edb4cdf8693fcd5f9c5f7bd45ab3d7f7a754
MulongXie <dsh15325@163.com>
2021-01-17 11:57:27 +1100

    fix rectangle recog bug by adding gap-to-prev

--- project/IP.py
+++ project/IP.py
@@ -41,30 +41,38 @@ def is_rectangle(contour):
     sides = []
     slopes = []
     side = [contour[0], contour[1]]
+    # variables for checking if it's valid to continue using the previous side
     pop_pre = False
-
+    gap_to_pre = 0
     for i, p in enumerate(contour[2:]):
         # calculate the slope k between two neighboor points
         if contour[i][0] == contour[i - 1][0]:
             k = 'v'
         else:
             k = (contour[i][1] - contour[i - 1][1]) / (contour[i][0] - contour[i - 1][0])
+        # print(side, k_pre, gap_to_pre)
         # check if the two points on the same side
         if k != k_pre:
             # leave out noises
             if len(side) < 4:
-                if len(sides) > 0 and not pop_pre:
-                    # continue using the last side
-                    if k == slopes[-1]:
-                        side = sides.pop()
-                        k = slopes.pop()
-                        pop_pre = True
+                # continue using the last side
+                if len(sides) > 0 and k == slopes[-1] \
+                        and not pop_pre and gap_to_pre < 4:
+                    side = sides.pop()
+                    side.append(p)
+                    k = slopes.pop()
+                    pop_pre = True
+                    gap_to_pre = 0
+                else:
+                    gap_to_pre += 1
+                    side = [p]
             # count as valid side and store it in sides
             else:
                 sides.append(side)
                 slopes.append(k_pre)
                 side = [p]
                 pop_pre = False
+                gap_to_pre = 0
             k_pre = k
         else:
             side.append(p)
