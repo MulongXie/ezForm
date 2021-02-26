beae15155bc82fc6480c28a96e58811067b1f77a
MulongXie <dsh15325@163.com>
2021-02-26 16:59:41 +1100

    add backlog

--- uied-ip/ip.py
+++
@@ -1,9 +0,0 @@
-import detect_compo.ip_region_proposal as ip
-
-key_params = {'min-grad': 10, 'ffl-block': 5, 'min-ele-area': 50, 'merge-contained-ele': True,
-              'max-word-inline-gap': 4, 'max-line-gap': 4}
-
-input_path_img = 'data/input/1.jpg'
-output_root = 'data/output'
-
-ip.compo_detection(input_path_img, output_root, key_params, resize_by_height=None, show=True)
