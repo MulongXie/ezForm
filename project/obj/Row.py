b503186ffe8d21e7d5550ec2185b9a46ff6ade14
MulongXie <dsh15325@163.com>
2021-03-05 16:43:10 +1100

    add match two rows

--- project/obj/Row.py
+++ project/obj/Row.py
@@ -1,10 +1,14 @@
 import cv2
+from obj.Table import Table
 
 
 class Row:
     def __init__(self, elements=None):
         self.elements = elements if elements is not None else []
         self.parent_table = None
+        if elements is not None and len(elements) > 0:
+            for ele in elements:
+                ele.in_row = self
 
     def add_element(self, element):
         element.in_row = self
@@ -16,8 +20,37 @@ class Row:
             self.add_element(ele)
         return self
 
-    def row_similarity(self, row_b):
-        pass
+    def match_rows(self, row_b, bias=3):
+        '''
+        Match tow rows bu checking continuously justified elements
+        If matched then extract the matched elements as new rows and return those two new rows
+        :param row_b:
+        :param bias:
+        :return:
+        '''
+        row_a_eles = self.elements
+        row_b_eles = row_b.elements
+        for i, re1 in enumerate(row_a_eles):
+            for j, re2 in enumerate(row_b_eles):
+                # if tow cells from the two rows match, take them as the start points and match forward
+                if abs(re1.location['left'] - re2.location['left']) < bias and abs(re1.location['right'] - re2.location['right']) < bias:
+                    col_start = [i, j]
+                    k = 1
+                    while (i + k) < len(row_a_eles) and (j + k) < len(row_b_eles):
+                        rek1 = row_a_eles[i + k]
+                        rek2 = row_b_eles[j + k]
+                        if abs(rek1.location['left'] - rek2.location['left']) < bias and abs(rek1.location['right'] - rek2.location['right']) < bias:
+                            k += 1
+                        else:
+                            break
+                    if k > 1:
+                        col_end = [i + k, j + k]
+                        row_matched_a = Row(elements=row_a_eles[col_start[0]: col_end[0]])
+                        row_matched_b = Row(elements=row_b_eles[col_start[1]: col_end[1]])
+                        return row_matched_a, row_matched_b
+                    else:
+                        break
+        return None
 
     def visualize_row(self, board, color=(0, 255, 0), line=2, show=False):
         for ele in self.elements:
