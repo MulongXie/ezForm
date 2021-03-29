1aa4d4f34e7d1c0869cd0dcdc31f476b42f6b532
MulongXie <dsh15325@163.com>
2021-03-29 15:03:06 +1100

    rename variables in camel format

--- project/server.js
+++ project/server.js
@@ -16,20 +16,20 @@ app.get('/',function(req,res){
     console.log("Connecting at", time.toLocaleString())
 });
 
-var uploaded_img_id = 0
+var uploadedImgId = 0
 app.post('/process', function (req, res) {
     console.log('\nStart processing')
-    uploaded_img_id ++
+    uploadedImgId ++
     let imgBase64 = req.body.img.replace(/^data:image.*base64,/, "");
-    let uploadPath = 'data/upload/' + uploaded_img_id.toString() + '.jpg'
+    let uploadPath = 'data/upload/upload-' + uploadedImgId.toString() + '.jpg'
     // upload form image
     fs.writeFile(uploadPath, imgBase64, 'base64', function (err) {
         if (! err){
             console.log('Upload image to', uploadPath)
-            let input_path_split = uploadPath.split('/');
-            let result_dir = './data/output/' + input_path_split[input_path_split.length - 1].split('.')[0];
-            let detection_result_img = result_dir + '/detection.jpg'
-            let generation_page = result_dir + '/xml.html'
+            let inputPathSplit = uploadPath.split('/');
+            let resultDir = 'data/output/' + inputPathSplit[inputPathSplit.length - 1].split('.')[0];
+            let detectionResultImg = resultDir + '/detection.jpg'
+            let generationPage = resultDir + '/xml.html'
             
             // processing form
             let processer = child_process.exec('python main.py ' + uploadPath,
@@ -43,7 +43,8 @@ app.post('/process', function (req, res) {
                     }
                     else {
                         console.log('Processing successfully');
-                        res.json({code:1, stdout:stdout, result_img:detection_result_img, result_page:generation_page})
+                        res.json({code:1, stdout:stdout, resultImg:detectionResultImg,
+                            resultPage:generationPage, inputImg: uploadPath})
                     }
                 });
             processer.on('exit', function () {
@@ -51,7 +52,7 @@ app.post('/process', function (req, res) {
             });
         }
         else {
-            uploaded_img_id --;
+            uploadedImgId --;
             console.log(err);
             res.json({code: 0})
         }
@@ -61,18 +62,18 @@ app.post('/process', function (req, res) {
 app.post('/submitData', function (req, res) {
     let data = req.body
 
-    let form_img_file = './data/input/3.jpg'
-    let input_path_split = form_img_file.split('/');
-    let result_dir = './data/output/' + input_path_split[input_path_split.length - 1].split('.')[0];
-    let filled_data_file = result_dir + '/input_data.json'
-    let filled_form_img = result_dir + '/filled.jpg'
+    let formImgFile = './data/input/3.jpg'
+    let inputPathSplit = formImgFile.split('/');
+    let resultDir = './data/output/' + inputPathSplit[inputPathSplit.length - 1].split('.')[0];
+    let filledDataFile = resultDir + '/input_data.json'
+    let filledFormImg = resultDir + '/filled.jpg'
 
     // store input data
-    fs.writeFile(filled_data_file, JSON.stringify(data, null, '\t'), function (err) {
+    fs.writeFile(filledDataFile, JSON.stringify(data, null, '\t'), function (err) {
         if (! err){
             console.log('data store')
             // fill the data on the form img
-            let processer = child_process.exec('python generation/fill.py ' + form_img_file,
+            let processer = child_process.exec('python generation/fill.py ' + formImgFile,
                 function (error, stdout, stderr) {
                     if (error){
                         console.log(stdout);
@@ -83,7 +84,7 @@ app.post('/submitData', function (req, res) {
                     }
                     else {
                         console.log('Filling form successfully');
-                        res.json({code:1, stdout:stdout, filled_form_img:filled_form_img})
+                        res.json({code:1, stdout:stdout, filledFormImg:filledFormImg})
                     }
                 });
             processer.on('exit', function () {
