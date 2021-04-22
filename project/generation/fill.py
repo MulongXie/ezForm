ce644187f426522024856a444122192bbdde5c4d
MulongXie <dsh15325@163.com>
2021-04-22 22:37:15 +1000

    adjust filling position

--- project/generation/fill.py
+++ project/generation/fill.py
@@ -26,7 +26,7 @@ result_img = output_dir + '/filled.jpg'
 for ipt in input_data:
     if ipt in input_loc:
         # print(input_data[ipt], input_loc[ipt])
-        cv2.putText(img, input_data[ipt], (input_loc[ipt][0]['left']+5, input_loc[ipt][0]['top']+10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,0,255), 1)
+        cv2.putText(img, input_data[ipt], (input_loc[ipt][0]['left']+3, input_loc[ipt][0]['top']+10), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0,0,255), 1)
 cv2.imwrite(result_img, img)
 # cv2.imshow('filled', img)
 # cv2.waitKey()
