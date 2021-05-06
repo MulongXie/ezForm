acf4ede286ec338ce91035464507f927e798cd35
MulongXie <dsh15325@163.com>
2021-05-06 12:50:18 +1000

    server supports pdf processing

--- project/server.js
+++ project/server.js
@@ -20,7 +20,7 @@ var uploadedImgId = 0
 app.post('/process', function (req, res) {
     console.log('\nStart processing')
     // Processing uploaded forms
-    if (req.body.inputType === 'base64'){
+    if (req.body.inputType === 'image'){
         uploadedImgId ++
         let imgBase64 = req.body.img.replace(/^data:image.*base64,/, "");
         let uploadPath = 'data/upload/upload' + uploadedImgId.toString() + '.jpg'
@@ -28,7 +28,24 @@ app.post('/process', function (req, res) {
         fs.writeFile(uploadPath, imgBase64, 'base64', function (err) {
             if (! err){
                 console.log('Upload image to', uploadPath)
-                processImg(uploadPath, res)
+                processImg(uploadPath, res, 'image')
+            }
+            else {
+                uploadedImgId --;
+                console.log(err);
+                res.json({code: 0})
+            }
+        })
+    }
+    else if (req.body.inputType === 'pdf'){
+        uploadedImgId ++
+        let pdf = req.body.img.replace(/^data:.*base64,/, "");
+        let uploadPath = 'data/upload/upload' + uploadedImgId.toString() + '.pdf'
+        // upload form image
+        fs.writeFile(uploadPath, pdf, 'base64', function (err) {
+            if (! err){
+                console.log('Upload image to', uploadPath)
+                processImg(uploadPath, res, 'pdf')
             }
             else {
                 uploadedImgId --;
@@ -38,38 +55,66 @@ app.post('/process', function (req, res) {
         })
     }
     else if (req.body.inputType === 'path'){
-        processImg(req.body.img, res)
+        processImg(req.body.img, res, 'image')
     }
-
 })
 
-function processImg(inputImgPath, res){
+function processImg(inputImgPath, res, type){
     console.log('Processing img:', inputImgPath)
     let imgName = inputImgPath.split('/')
     imgName = imgName[imgName.length - 1].split('.')[0]
-    let resultDir = 'data/output/' + imgName;
-    let detectionResultImg = resultDir + '/detection.jpg'
-    let generationPage = resultDir + '/xml.html'
-    let compoLocFile = resultDir + '/input_loc.json'
-
-    // processing form
-    let processer = child_process.exec('python main.py ' + inputImgPath,
-        function (error, stdout, stderr) {
-            if (error){
-                console.log(stdout);
-                console.log(error.stack);
-                console.log('Error code: '+error.code);
-                console.log('Signal received: '+error.signal);
-                res.json({code:0});
-            }
-            else {
-                console.log('Processing successfully');
-                res.json({code:1, resultImg:detectionResultImg, resultPage:generationPage, inputImg: inputImgPath, compoLocFile: compoLocFile})
-            }
+    if (type === 'image'){
+        let resultDir = 'data/output/' + imgName;
+        let detectionResultImg = resultDir + '/detection.jpg'
+        let generationPage = resultDir + '/xml.html'
+        let compoLocFile = resultDir + '/input_loc.json'
+        // processing form
+        let processer = child_process.exec('python main-pdf.py ' + inputImgPath,
+            function (error, stdout, stderr) {
+                if (error){
+                    console.log(stdout);
+                    console.log(error.stack);
+                    console.log('Error code: '+error.code);
+                    console.log('Signal received: '+error.signal);
+                    res.json({code:0});
+                }
+                else {
+                    console.log('Processing successfully');
+                    res.json({code:1, resultImg:detectionResultImg, resultPage:generationPage, inputImg: inputImgPath, compoLocFile: compoLocFile})
+                }
+            });
+        processer.on('exit', function () {
+            // console.log('Program Completed');
         });
-    processer.on('exit', function () {
-        // console.log('Program Completed');
-    });
+    }
+
+    else if (type === 'pdf'){
+        let resultDir = 'data/output/pdf-' + imgName;
+        // processing form
+        let processer = child_process.exec('python main-pdf.py ' + inputImgPath,
+            function (error, stdout, stderr) {
+                if (error){
+                    console.log(stdout);
+                    console.log(error.stack);
+                    console.log('Error code: '+error.code);
+                    console.log('Signal received: '+error.signal);
+                    res.json({code:0});
+                }
+                else {
+                    // console.log(stdout);
+                    console.log(JSON.parse(stdout.replace(/'/g, '"')))
+                    let detectionResultImg = resultDir + '/detection.jpg'
+                    let generationPage = resultDir + '/xml.html'
+                    let compoLocFile = resultDir + '/input_loc.json'
+                    console.log('Processing successfully');
+                    // res.json({code:1, resultImg:detectionResultImg, resultPage:generationPage, inputImg: inputImgPath, compoLocFile: compoLocFile})
+                    res.json({code:0});
+                }
+            });
+        processer.on('exit', function () {
+            // console.log('Program Completed');
+        });
+    }
 }
 
 app.post('/fillForm', function (req, res) {
